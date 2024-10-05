2796d0360fd4fc9a797c368f6551923aa8ce570e
yigit <yigit@YIIT>
2024-10-05 18:17:34 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region string degiskenler
+            /*
+            string ad = " Yiğit Efe Çakan";
+            string meslek = "Bilgisayar Mühendisligi";
+            string cinsiyet = "Erkek";
+            Console.WriteLine("ad:" + ad);
+            Console.WriteLine("meslek:" + meslek);
+            Console.WriteLine("cinsiyet:" + cinsiyet);
+            */
+            #endregion
+
+            #region ınt degiskenler
+            /*
+            int appleprice = 1;
+            int bananaprice = 2;
+            int milkprice = 3;
+            int onionprice = 4;
+            int applecount = 2;
+            int bananacount = 2;
+            int milkcount = 2;
+            int onioncount = 2;
+
+            int totalprice = (appleprice * applecount) + (bananaprice * bananacount) + (milkprice * milkcount) + (onionprice * onioncount) ;
+            Console.WriteLine("total price : " + totalprice);
+            */
+            #endregion
+
+            #region double degiskenler
+            /*
+            double appleprice = 1;
+            double bananaprice = 2;
+            double milkprice = 3;
+            double onionprice = 4;
+            double applecount = 2;
+            double bananacount = 2;
+            double milkcount = 2;
+            double onioncount = 2;
+
+            double totalprice = (appleprice * applecount) + (bananaprice * bananacount) + (milkprice * milkcount) + (onionprice * onioncount);
+            Console.WriteLine("total price : " + totalprice);
+            */
+            #endregion
+
+            #region klavyeden veri girisleri string degiskenler
+            /*
+            string username,age,gender;
+            Console.WriteLine("please enter your username");
+            username = Console.ReadLine();
+            Console.WriteLine("please enter your age");
+            age = Console.ReadLine();
+            Console.WriteLine("please enter your gender");
+            gender = Console.ReadLine();
+
+            Console.WriteLine("Username : " + username+" "+ "Age : " + age + " " + "Gender : " + gender);
+            */
+            #endregion
+
+            #region klavyeden veri girisleri int degiskenler
+
+            #endregion
+
+            Console.Read();
+
+        }
+    }
+}
